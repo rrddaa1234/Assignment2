3077d89e245007d124f0920960fbf0bb08f2c35f
342889045 <342889045@C-5W0XS13.tdsb.on.ca>
2026-05-14 10:38:06 -0400

    hi - Bryan

---
+++ src/ethicsprogram/EthicsProgram.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ethicsprogram;
+
+/**
+ *
+ * @author 342889045
+ */
+public class EthicsProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
