a155666566d48269430d9925626f5205a42632a0
342987914 <342987914@C-5VXTS13.tdsb.on.ca>
2026-05-29 09:41:53 -0400

    made empty file - keming

--- src/ethicsprogram/EthicalFileInput.java
+++ src/ethicsprogram/EthicalFileInput.java
@@ -12,20 +12,20 @@ import java.io.IOException;
  * @author 342987914
  */
 public class EthicalFileInput {
-    public static void main(String[] args) {
-        try {
-            Scanner sc = new Scanner(new File("filewithstuff"));
-            while (sc.hasNext()) {
-                String output = sc.nextLine();
-                String[] info = output.split(",");
-                System.out.println("Enter: ");
+    //public static void main(String[] args) {
+        //try {
+            //Scanner sc = new Scanner(new File("filewithstuff"));
+            //while (sc.hasNext()) {
+            //    String output = sc.nextLine();
+            //    String[] info = output.split(",");
+                //System.out.println("Enter: ");
                 
-            }
-            sc.close();
-        }
+            //}
+            //sc.close();
+        //}
 
-        catch (IOException ioException) {
+        //catch (IOException ioException) {
             
-        }
-    }
+        //}
+    //}
 }
